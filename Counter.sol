677d16ba74e41f116c0ee7a051a2962b22d790ec
Diogo Sequeira <diogosequeira@hotmail.fr>
2021-08-28 23:22:11 +0100

    added functions and first contract

--- Counter.sol
+++ Counter.sol
@@ -1,9 +1,22 @@
-Counter.sol
+pragma solidity ^0.8.7;
 
 contract Counter {
     uint count;
+
+    // Constructor syntax
+    constructor() public {
+        count = 0;
+    }
     
-    function getCount() public view {
+    /// Method syntax, [public] means visible outside contract
+    /// [view] means it's only a getter
+    /// returs(uint) to specify return type
+    function getCount() public view returns(uint) {
         return count;
+    }
+
+    function incrementCount() public {
+        // count = count + 1;
+        count++;
     }
 }
