8c58ed72695cc5f98326db069e943337a8613dfa
Diogo Sequeira <diogosequeira@hotmail.fr>
2021-08-30 13:21:34 +0100

    added receive function and smart contract

--- HotelRoom.sol
+++ HotelRoom.sol
@@ -13,6 +13,8 @@ contract HotelRoom {
     }
     Statuses currentStatus;
 
+    event Occupy(address _ocupant, uint256 _value);
+
     /// [payable] the address can receive ethereum as payment
     address payable public owner;
 
@@ -22,10 +24,15 @@ contract HotelRoom {
         currentStatus = Statuses.Vacant;
     }
 
-    // Modifiers can be used to check something before running the function
+    /// Modifiers can be used to check something before running the function
     modifier onlyWhileVacant() {
         require(currentStatus == Statuses.Vacant, "Currently Ocuppied.");
         _;
+    }
+
+    /// We could also use another modifier for [bookRoom] (costs(2 ether))
+    modifier costs(uint256 _amount) {
+        require(msg.value >= _amount, "Not enough Ether provided.");
     }
 
     function bookRoom() payable onlyWhileVacant {
@@ -35,5 +42,14 @@ contract HotelRoom {
         require(msg.value >= 2 ether, "Not enough Ether provided.");
         currentStatus = Statuses.Occupied;
         owner.transfer(msg.value);
+        emit Occupy(msg.sender, msg.value);
+    }
+
+    /// receive function, like an anonymous, same code as above
+    receive() external payable onlyWhileVacant {
+        require(msg.value >= 2 ether, "Not enough Ether provided.");
+        currentStatus = Statuses.Occupied;
+        owner.transfer(msg.value);
+        emit Occupy(msg.sender, msg.value);
     }
 }
