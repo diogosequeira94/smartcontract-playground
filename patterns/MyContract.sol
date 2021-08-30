06b7a4a4fa71ac488fe18343556814677a8fd5f2
Diogo Sequeira <diogosequeira@hotmail.fr>
2021-08-30 13:36:56 +0100

    adds a new smart contract with design patterns

---
+++ patterns/MyContract.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.6.0;
+
+contract MyContract {
+    string secret;
+
+    // only the onwer of the SC can call it
+    modifier onlyOwner() {
+        require(msg.sender == owner, "must be owner");
+        _;
+    }
+
+    constructor(string memory _secret) public {
+        secret = _secret;
+        owner = msg.sender;
+    }
+
+    function getSecret() public view onlyOwner returns (string memory) {
+        return secret;
+    }
+}
